8e81ef587fcaee69c0366770b319d70d7dc0d69d
lave <594213482@qq.com>
2018-05-06 15:29:29 +0800

    modified productImage

--- AdpStore/Models/Order.cs
+++ AdpStore/Models/Order.cs
@@ -14,6 +14,7 @@ namespace AdpStore.Models
         public string ProductName { get; set; }
         public int Quantity { get; set; }
         public decimal OrderPrice { get; set; }
+        public string ImageName { get; set; }
         public DateTime Indate { get; set; }
     }
 }
